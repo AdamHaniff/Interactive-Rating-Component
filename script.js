e70dc581d13e9304b35f7b43023c88d89d0ca7d2
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2023-06-27 16:42:38 -0400

    Styled desktop version & added some JavaScript

--- script.js
+++ script.js
@@ -2,3 +2,18 @@
 
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+
+// when a number is clicked, we want that number to have an orange background.
+// we will use an event listener for this - maybe use event propagation on the parent container so that we dont have to add a function on each individual element.
+// if an element is already clicked and we click another one, we need to reset to default state and have that new element clicked
+
+const ratingNumbersContainer = document.querySelector(
+  ".rating-numbers-container"
+);
+
+ratingNumbersContainer.addEventListener("click", function (e) {
+  const number = e.target.closest(".number");
+  console.log(number);
+  number.classList.add("clicked");
+  number.classList.remove("number_hover");
+});
